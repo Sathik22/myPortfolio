d235e43fb6836e8a198785987272fe93817f39bd
Sathik22 <mohamedjahabarsathik2001@gmail.com>
2024-12-16 14:38:45 +0530

    Portfolio Updated

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// Select the necessary elements
+const hamburger = document.querySelector('.hamburger');
+const menuLinks = document.querySelector('.menu-links');
+const menuBarIcon = document.querySelector('.menuBar');
+const closeMenuIcon = document.querySelector('.closeMenu');
+
+closeMenuIcon.style.display = 'none';
+
+
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  
+  if (menuLinks.style.display === 'flex') {
+
+    menuLinks.style.display = 'none';
+    menuBarIcon.style.display = 'block'; 
+    closeMenuIcon.style.display = 'none'; 
+  } else {
+    
+    menuLinks.style.display = 'flex';
+    menuBarIcon.style.display = 'none'; 
+    closeMenuIcon.style.display = 'block'; 
+  }
 }
+
+hamburger.addEventListener('click', toggleMenu);
